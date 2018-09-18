25801b6fd31c9276c31a398b358443a12d606eb5
wildanjisung <wildanjisung@gmail.com>
2018-09-18 15:36:08 +0700

    'berhasil_tambah_data_tables'

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -1,8 +1,20 @@
 <!DOCTYPE html>
 <html>
 <head>
+	<!--  -->
 	<title>Code Igniter Tutorial</title>
 	<link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/css/bootstrap.css') ?>">
+	<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.19/css/jquery.dataTables.min.css"> <!-- css for data tables -->
+	
+	<!-- js for datatables -->
+	<script type="text/javascript" src="https://code.jquery.com/jquery-3.3.1.js"></script>
+	<script type="text/javascript" src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js"></script>
+	<script type="text/javascript" language="javascript" class="init">
+		$(document).ready(function() {
+		    $('#example').DataTable();
+		} );
+	</script>
+	<!-- end js for datatables -->
 </head>
 <body>
 	<div class="container">
